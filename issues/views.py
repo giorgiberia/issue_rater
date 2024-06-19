f6ddda6ee2174960b2745430c9fcb0964f9c3a4b
Gio <beria.giorgi1@gmail.com>
2024-06-19 15:20:44 +0400

    added pagination and desc to table

--- issues/views.py
+++ issues/views.py
@@ -1,30 +1,24 @@
-from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
-from .models import Issue, Vote
-from .forms import IssueForm, VoteForm
-
-
-# views.py
-
+from django.core.paginator import Paginator
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import login_required
-from .models import Issue, Vote
+
 from .forms import IssueForm, VoteForm
+from .models import Issue
 
-
-@login_required
 def issue_list_and_create(request):
     if request.method == "POST":
         form = IssueForm(request.POST)
         if form.is_valid():
-            issue = form.save(commit=False)
-            issue.rating = 0  # Ensure the rating is set to 0 by default
-            issue.save()
+            form.save()
             return redirect("issue_list_and_create")
     else:
         form = IssueForm()
 
-    issues = Issue.objects.all()
+    issues_list = Issue.objects.all()
+    paginator = Paginator(issues_list, 7)  # Show 10 issues per page
+    page_number = request.GET.get("page")
+    issues = paginator.get_page(page_number)
+
     vote_form = VoteForm()
     return render(
         request,
@@ -36,7 +30,7 @@ def issue_list_and_create(request):
 @login_required
 def vote_issue(request, issue_id):
     issue = get_object_or_404(Issue, pk=issue_id)
-    if request.method == 'POST':
+    if request.method == "POST":
         form = VoteForm(request.POST)
         if form.is_valid():
             # Ensure the current user is passed to the form's save method
@@ -44,5 +38,5 @@ def vote_issue(request, issue_id):
                 form.save(issue=issue, user=request.user)
             except Exception as e:
                 print(f"Error saving vote: {e}")
-            return redirect('issue_list_and_create')
-    return redirect('issue_list_and_create')
+            return redirect("issue_list_and_create")
+    return redirect("issue_list_and_create")
