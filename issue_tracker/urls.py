5a3dc05a1a9343e3aaf64a5c8fba336f042209fe
Gio <beria.giorgi1@gmail.com>
2024-06-24 15:59:05 +0400

    add statics to url

--- issue_tracker/urls.py
+++ issue_tracker/urls.py
@@ -14,12 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include   # Include 'include' for the URLs
+from django.urls import path, include  # Include 'include' for the URLs
 
 from issues.Public.views import public_page
-from issues.views import register, user_login, user_logout, issue_list_and_create
+from issues.views import register, user_login, user_logout
 
 urlpatterns = [
     path('', public_page, name='public_page'),
@@ -30,5 +31,9 @@ urlpatterns = [
     path('login/', user_login, name='login'),
     path('logout/', user_logout, name='logout'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 
